a755f5772315f94df2ad0c11fb6680a120b2bab5
angelgbd <baeltranangel@gmail.com>
2025-12-04 09:32:42 -0700

    Initial commit del proyecto AdministrarCitasPrincipal

---
+++ src/main/java/com/example/administrarcitas/AdministrarCitas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.administrarcitas;
+
+/**
+ *
+ * @author angel
+ */
+public class AdministrarCitas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
